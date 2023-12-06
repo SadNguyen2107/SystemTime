0681ac5fe04b4e4281d01d48d42a0634e789d59c
Sad <nguyentrungson217@gmail.com>
2023-12-06 20:56:58 +0700

    HomeWork Week2

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            Console.WriteLine("Datetime Now is: " + localDate);
+        }
+    }    
+}
